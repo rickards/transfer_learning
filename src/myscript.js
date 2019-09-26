05d7b4a5c47a755cad15bc47978abbb0307b6da7
Joab <jlsn1@ifal.edu.br>
2019-09-26 15:43:44 -0300

    Canvas: Use canvas each frame

--- src/myscript.js
+++ src/myscript.js
@@ -1,25 +1,37 @@
-const loadCamera = () => {
-  //Captura elemento de vídeo
-  var video = document.querySelector("#webCamera");
-  //As opções abaixo são necessárias para o funcionamento correto no iOS
-  video.setAttribute('autoplay', '');
-  video.setAttribute('muted', '');
-  video.setAttribute('playsinline', '');
-  //--
+const videoWidth = 500;
+const videoHeight = 500;
+const video_test = document.getElementById('webCamera');
+video_test.width = videoWidth;
+video_test.height = videoHeight;
+const color = "#FFFF";
 
-  //Verifica se o navegador pode capturar mídia
-  if (navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({ audio: false, video: { facingMode: 'user' } })
-      .then(function (stream) {
-        //Definir o elemento vídeo a carregar o capturado pela webcam
-        video.srcObject = stream;
-      })
-      .catch(function (error) {
-        alert("Oooopps... Falhou :'(");
-      });
-  }
+async function setupWebcam() {
+  return new Promise((resolve, reject) => {
+    const navigatorAny = navigator;
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+        navigatorAny.msGetUserMedia;
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia({video: true},
+          stream => {
+            video_test.srcObject = stream;
+            video_test.addEventListener('loadeddata', () => resolve(), false);
+          },
+          error => reject());
+    } else {
+      reject();
+    }
+  });
 }
 
+
+function drawPoint(ctx, y, x, r, color, part) {
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, 10 * Math.PI);
+  ctx.fillStyle = color;
+  // ctx.fillText(part, x, y, 500);
+  ctx.fill();
+}
 const takeSnapShot = () => {
   //Captura elemento de vídeo
   var video = document.querySelector("#webCamera");
@@ -29,6 +41,7 @@ const takeSnapShot = () => {
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
   var ctx = canvas.getContext('2d');
+  drawPoint(ctx, 200, 200, 1.0, 'black', 1);
 
   //Desenhando e convertendo as dimensões
   ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
@@ -50,6 +63,48 @@ const download = (imageUrl) => {
   document.body.removeChild(link);
 }
 
+
+async function app() {
+
+  await setupWebcam();
+  const canvas = document.getElementById('output');
+  canvas.width = videoWidth;
+  canvas.height = videoHeight;
+  console.log(canvas);
+  const modelHandTrackParams = {
+    flipHorizontal: false,   // flip e.g for video
+    imageScaleFactor: 0.7,  // reduce input image size .
+    maxNumBoxes: 1,        // maximum number of boxes to detect
+    iouThreshold: 0.5,      // ioU threshold for non-max suppression
+    scoreThreshold: 0.35,    // confidence threshold for predictions.
+  };
+  const model = await handTrack.load(modelHandTrackParams);
+
+
+  while (true) {
+
+    const ctx = canvas.getContext('2d');
+    model.detect(video_test).then(predictions => {
+      console.log('Predictions: ', predictions) // bbox predictions
+      ctx.drawImage(video_test, 0, 0, videoWidth, videoHeight);
+
+      ctx.beginPath();
+      const test = predictions[0] ? predictions[0].bbox : [0,0,560,560];
+
+      ctx.strokeStyle = 'green';
+      if(test.length != 0){
+        ctx.rect(test[0], test[1], test[2], test[3]);
+      }
+
+      ctx.stroke();
+    });
+
+    // drawKeypoints(pose.keypoints, minPoseConfidence, ctx);
+    await tf.nextFrame();
+  }
+
+}
+
 window.onload = () => {
-  loadCamera();
+  app();
 };
