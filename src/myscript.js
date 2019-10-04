2b9a0c0912bcd2a44da00961f279855134f768fe
rickards <henrique.aracomp@gmail.com>
2019-10-04 17:24:47 -0300

    Melhora performance da detecção da mão

--- src/myscript.js
+++ src/myscript.js
@@ -7,21 +7,29 @@ const color = "#FFFF";
 const image_label_front = document.getElementById('hand_front');
 const image_label_side = document.getElementById('hand_side');
 
+const modelHandTrackParams = {
+  flipHorizontal: false,   // flip e.g for video
+  imageScaleFactor: 0.7,  // reduce input image size .
+  maxNumBoxes: 1,        // maximum number of boxes to detect
+  iouThreshold: 0.5,      // ioU threshold for non-max suppression
+  scoreThreshold: 0.35,    // confidence threshold for predictions.
+};
+
 var n_image = 1
 
 async function setupWebcam() {
   return new Promise((resolve, reject) => {
     const navigatorAny = navigator;
     navigator.getUserMedia = navigator.getUserMedia ||
-        navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
-        navigatorAny.msGetUserMedia;
+      navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+      navigatorAny.msGetUserMedia;
     if (navigator.getUserMedia) {
-      navigator.getUserMedia({video: true},
-          stream => {
-            video_test.srcObject = stream;
-            video_test.addEventListener('loadeddata', () => resolve(), false);
-          },
-          error => reject());
+      navigator.getUserMedia({ video: true },
+        stream => {
+          video_test.srcObject = stream;
+          video_test.addEventListener('loadeddata', () => resolve(), false);
+        },
+        error => reject());
     } else {
       reject();
     }
@@ -30,8 +38,8 @@ async function setupWebcam() {
 
 const nextLabel = () => {
   n_image = n_image + 1
-  image_label_front.src = "../images/Front/"+n_image+".png"
-  image_label_side.src = "../images/Side/"+n_image+".png"
+  image_label_front.src = "../images/Front/" + n_image + ".png"
+  image_label_side.src = "../images/Side/" + n_image + ".png"
 }
 
 const takeSnapShot = () => {
@@ -57,7 +65,7 @@ const download = (imageUrl) => {
 
   var link = document.createElement('a');
   link.href = imageUrl;
-  link.download = "mao_"+n_image+"_"+document.getElementById('input_name').value+".jpg";
+  link.download = "mao_" + n_image + "_" + document.getElementById('input_name').value + ".jpg";
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
@@ -66,34 +74,43 @@ const download = (imageUrl) => {
 async function app() {
 
   await setupWebcam();
+
+  const model = await handTrack.load(modelHandTrackParams);
+
+  let isHandTrackLocked = false;
+  let lastResult;
+  const useHandTracking = async (video_test) => {
+    if (isHandTrackLocked) return lastResult;
+    isHandTrackLocked = true;
+
+    // Limita a execução do handtrack a 500 milisegundos por vez
+    setTimeout(async () => {
+      let test = [0, 0, videoWidth, videoHeight];
+      const predictions = await model.detect(video_test);
+      test = predictions[0] ? predictions[0].bbox : [0, 0, videoWidth, videoHeight];
+      lastResult = test;
+      isHandTrackLocked = false;
+    }, 100);
+    return lastResult;
+  }
+
   const canvas = document.getElementById('output');
   canvas.width = videoWidth;
   canvas.height = videoHeight;
-  
-  const modelHandTrackParams = {
-    flipHorizontal: false,   // flip e.g for video
-    imageScaleFactor: 0.7,  // reduce input image size .
-    maxNumBoxes: 1,        // maximum number of boxes to detect
-    iouThreshold: 0.5,      // ioU threshold for non-max suppression
-    scoreThreshold: 0.35,    // confidence threshold for predictions.
-  };
-  const model = await handTrack.load(modelHandTrackParams);
 
-  const update = () => {
+  const update = async () => {
     const ctx = canvas.getContext('2d');
-    
+
     ctx.drawImage(video_test, 0, 0, videoWidth, videoHeight);
 
-    model.detect(video_test).then(predictions => {
-      
+    let test = await useHandTracking(video_test);
+    if (test) {
       ctx.strokeStyle = 'green';
       ctx.beginPath();
-      const test = predictions[0] ? predictions[0].bbox : [0,0,videoWidth,videoHeight];
 
       radio_improve = 25
-      ctx.rect(test[0]-radio_improve, test[1]-radio_improve, test[2]+radio_improve*2, test[3]+radio_improve*2);
-      
-    });
+      ctx.rect(test[0] - radio_improve, test[1] - radio_improve, test[2] + radio_improve * 2, test[3] + radio_improve * 2);
+    }
 
     ctx.stroke();
 
